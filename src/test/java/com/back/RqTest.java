de9e5cea915b5f747999e648b26a935f102e7541
HaheeBahee <parkadg@naver.com>
2025-09-04 01:09:43 +0900

    Green1

--- src/test/java/com/back/RqTest.java
+++ src/test/java/com/back/RqTest.java
@@ -1,9 +1,13 @@
 package com.back;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RqTest {
 
     @Test
-    @Displayname("getActionName")
+    @DisplayName("getActionName(\"삭제?id=1\") : 삭제")
     void t1(){
         Rq rq = new Rq("삭제?id=1");
         String actionName = rq.getActionName(); //삭제
