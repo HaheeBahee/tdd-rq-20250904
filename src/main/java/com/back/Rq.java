8a2587d8f125c4dc4bfdbf94eac8dba702727c06
HaheeBahee <parkadg@naver.com>
2025-09-04 11:12:56 +0900

    Blue7

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -4,12 +4,12 @@ public class Rq {
 
     private String cmd;
 
-    public Rq(String cmd){
+    public Rq(String cmd) {
         this.cmd = cmd;
     }
 
 
-    public String getActionName(){
+    public String getActionName() {
 
         //?기준 왼쪽을 잘라 반환 -> 자르는 방법 gpt한테 물어보자 -> split
 
@@ -18,22 +18,20 @@ public class Rq {
     }
 
     public String getParam(String inputKey, String defaultValue) {
+        String[] cmdBits = cmd.split("\\?", 2);
+        if (cmdBits.length < 2) return defaultValue;
 
-        if(cmd.equals("등록?고향=서울&이름=홍길동")&&inputKey.equals("고향"))return "서울";
-        if(cmd.equals("등록?고향=서울&이름=홍길동")&&inputKey.equals("이름"))return "홍길동";
+        String queryString = cmdBits[1]; // 고향=서울&이름=홍길동
+        String[] queryBits = queryString.split("&");
 
-
-        String[] cmdBits = cmd.split("\\?");
-        String queryString = cmdBits[1]; //이름=홍길동
-
-        String[] paramBits = queryString.split("=");
-        String key = paramBits[0];
-        String value = paramBits[1];
-
-        if(inputKey.equals(key)){
-            return value;
+        for (String param : queryBits) {
+            String[] paramBits = param.split("=");
+            String key = paramBits[0];
+            String value = paramBits[1];
+            if (inputKey.equals(key)) {
+                return value;
+            }
         }
-
         return defaultValue;
     }
 }
